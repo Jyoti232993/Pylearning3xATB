53a700d7b5a214ede811af7ba79c63f4e18b416a
jyotiparmar <jyotiparmar1993@gmail.com>
2024-06-06 19:03:30 +0530

    Third lecture - Python variables and data types

---
+++ Ex_2-05-06-2024/LAB_003.py
@@ -0,0 +1,11 @@
+# print()
+# self - Concept in OOPS which points to itself - ignore.
+# *args - Unlimited number of arguments * - string, int, float, boolean...
+# sep=' ' - How you want to separate the arguments
+# end='\n' - in the end what you want to do
+# file=None - File IO
+
+
+print("Hello", "World", 123, True, 3.14, sep="-")
+print("Hi, My name is Jyoti", "Parmar", sep="-")
+
---
+++ Ex_2-05-06-2024/LAB_005.py
@@ -0,0 +1,5 @@
+# print(sep="-", "Hi", "Jyoti")
+print("I am a good person", end="_")
+print("I am a bad person")
+# Python is case sensitive
+#Print("Hi")
