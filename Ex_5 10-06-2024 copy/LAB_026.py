d39a3c2e6deb958c8ad32a2a684771df825404c9
jyotiparmar <jyotiparmar1993@gmail.com>
2024-06-11 21:44:10 +0530

    Operators and literals

---
+++ Ex_5 10-06-2024 copy/LAB_026.py
@@ -0,0 +1,7 @@
+my_set = {1, 2, 3, 4, 5, 5} # Collection of Unique items
+print(my_set)
+
+newline_char = "\n"
+newline_char2 = "\t"
+newline_char2 = "\b" # backspace
+
---
+++ Ex_5 10-06-2024 copy/LAB_032.py
@@ -0,0 +1,13 @@
+# Ternary Operator
+jyoti_marks = 90
+karan_marks = 97
+
+#   x > y -> do something - print("pramod")
+# y > x -> do something else -> print("amit)
+
+print("Jyoti is winner" if jyoti_marks > karan_marks else "karan is winner")
+
+if (jyoti_marks > karan_marks):
+    print("Jyoti is winner")
+else:
+    print("Karan is winner")
