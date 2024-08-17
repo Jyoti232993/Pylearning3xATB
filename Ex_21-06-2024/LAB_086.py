2c81dad09e6292dc5180e206cc7eb2cc02607e09
JYOTI <jyotiparmar1993@gmail.com>
2024-08-17 19:04:12 +0530

    Delete Ex_21-06-2024 directory

--- Ex_21-06-2024/LAB_086.py
+++
@@ -1,9 +0,0 @@
-t = tuple()
-print(t)
-
-# Conversion List to Tuple
-t1 = tuple(["Pramod", "Amit", "Manisha"])
-print(t1)
-
-del(t1)
-# print(t1)
--- Ex_21-06-2024/Task 6/Task 6c.py
+++
@@ -1,7 +0,0 @@
-# Program to reverse a string
-
-string_input = input("Enter a string: ")
-
-reverse_string = string_input[::-1]
-print("The actual string is: ", string_input)
-print("The reverse string is: ", reverse_string)
